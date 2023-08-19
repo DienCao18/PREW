b55bbb671520e50fabbd72209a7187231c4e8791
Dien Cao <dien.cm@bliss-interactive.com>
2023-08-19 14:27:13 +0700

    up

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,7 +1,63 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+
 
 export default function footer() {
   return (
-    <div>footer</div>
+    <footer className={styles.footer}>
+      <div className={styles.footer_block}>
+        <div className={styles.footer_block_detail}>
+          <img src='/images/footer_logo.png'></img>
+          <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang. Lorem Ipsum đã được sử dụng như một văn bản. </p>
+        </div>
+        <div className={styles.footer_block_detail}>
+          <h6>Về PREW</h6>
+          <ul>
+            <li><a href='#'>Giới thiệu</a></li>
+            <li><a href='#'>Tài liệu</a></li>
+            <li><a href='#'>Thi thử online</a></li>
+            <li><a href='#'>Khóa học online</a></li>
+            <li><a href='#'>Tin tức tuyển sinh</a></li>
+            <li><a href='#'>Liên hệ</a></li>
+          </ul>
+        </div>
+        <div className={styles.footer_block_detail}>
+          <h6>Liên hệ với chúng tôi</h6>
+          <div className={styles.footer_block_detail_info}>
+            <img src='/images/address.png'></img>
+            <p>2/102 Lê Thanh Nghị, quận <br /> Hai Bà Trưng, Hà Nội</p>
+          </div>
+          <div className={styles.footer_block_detail_info}>
+            <img src='/images/email.png'></img>
+            <p>prew@gmail.com.vn</p>
+          </div>
+          <div className={styles.footer_block_detail_info}>
+            <img src='/images/phone.png'></img>
+            <p>0973844646</p>
+          </div>
+        </div>
+        <div className={styles.footer_block_detail}>
+          <h6>Nhận thông báo bảng tin mới</h6>
+          <div className={styles.footer_block_detail_infomation}>
+            <p>Điền email của bạn để PREW gửi tin mới nhé!</p>
+            <div className={styles.email}>
+              <input type='text' placeholder='Email của bạn' />
+              <img src='/images/plane_paper.png'></img>
+            </div>
+            <div className={styles.social}>
+              <p>Theo dõi chúng tôi:</p>
+
+              <img src='/images/social.png'></img>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer_brand}>
+        <img src='/images/bear.png'></img>
+        <p>MONA.Media/ Website+1</p>
+      </div>
+    </footer>
   )
 }
--- components/students/students.js
+++ components/students/students.js
@@ -1,32 +1,124 @@
-import React from 'react'
+import React from 'react';
 import styles from './Students.module.scss'
 
-export default function students() {
+
+
+
+
+export default function Students() {
+  
+ 
   return (
-    <div className={styles.students}>
-      <div className={styles.students_bg}>
-        <img src='/images/world.png'></img>
+    <>
+      <div className={styles.students}>
+        <div className={styles.students_bg}>
+          <img src='/images/world.png'></img>
+
+        </div>
+        <div className={styles.students_bg_rignt}>
+          <img src='/images/bg_right.png'></img>
+
+        </div>
+
+        <div className={styles.students_title}>
+          <h3> Học sinh - sinh viên nói gì về chúng tôi</h3>
+        </div>
+        <div className={styles.students_content}>
+          <p>Nhờ có Prew mà mình đã đạt được target 800+ Toeic trong 6 tháng. Mình thấy Prew là một trang Web luyện thi Toeic cực hiệu quả lại còn miễn phí nữa nên là các bạn nào có dự tính tự học thì mau mau đăng ký tài khoản để học nha. Cảm ơn Prew rất nhiều &lt;3</p>
+          <span>Trâm Anh <br />Thành viên Toeic của Prew</span>
+        </div>
+        <div className={styles.students_nhai}>
+          <img src='/images/nhai.png'></img>
+
+        </div>
+        <div className={styles.student_girl}>
+          <img src='/images/student_girl.png'></img>
+        </div>
+
+        <div className={styles.student_1}>
+          <img src='/images/student1.png'></img>
+        </div>
+        <div className={styles.student_2}>
+          <img src='/images/student2.png'></img>
+        </div>
+        <div className={styles.student_3}>
+          <img src='/images/student3.png'></img>
+        </div>
+        <div className={styles.student_4}>
+          <img src='/images/student4.png'></img>
+        </div>
+        <div className={styles.student_5}>
+          <img src='/images/student5.png'></img>
+        </div>
+
+        <div className={styles.student_6}>
+          <img src='/images/student1.png'></img>
+        </div>
+        <div className={styles.student_7}>
+          <img src='/images/student2.png'></img>
+        </div>
+        <div className={styles.student_8}>
+          <img src='/images/student3.png'></img>
+        </div>
+        <div className={styles.student_9}>
+          <img src='/images/student4.png'></img>
+        </div>
+        <div className={styles.student_10}>
+          <img src='/images/student5.png'></img>
+        </div>
+        <div className={styles.student_11}>
+          <img src='/images/student1.png'></img>
+        </div>
+        <div className={styles.student_12}>
+          <img src='/images/student14.png'></img>
+        </div>
+        <div className={styles.student_13}>
+          <img src='/images/student3.png'></img>
+        </div>
+        <div className={styles.student_14}>
+          <img src='/images/student14.png'></img>
+        </div>
+        <div className={styles.student_15}>
+          <img src='/images/student15.png'></img>
+        </div>
+
 
       </div>
 
-      <div className={styles.students_title}>
-        <h3> Học sinh - sinh viên nói gì về chúng tôi</h3>
-      </div>
-      <div className={styles.students_content}>
-        <p>Nhờ có Prew mà mình đã đạt được target 800+ Toeic trong 6 tháng. Mình thấy Prew là một trang Web luyện thi Toeic cực hiệu quả lại còn miễn phí nữa nên là các bạn nào có dự tính tự học thì mau mau đăng ký tài khoản để học nha. Cảm ơn Prew rất nhiều &lt;3</p>
-        <span>Trâm Anh <br />Thành viên Toeic của Prew</span>
-      </div>
-      <div className={styles.students_nhai}>
-        <img src='/images/nhai.png'></img>
+      <div className={styles.company}>
+        <div className={styles.company_title}>
 
-      </div>
-      <div className={styles.student_girl}>
-        <img src='/images/student_girl.png'></img>
-      </div>
+          <h3>Đối tác của chúng tôi</h3>
+          <div className={styles.company_title_button}>
+            <button className={styles.prev_button}>&lt;</button>
+            <button className={styles.next_button}>&gt;</button>
+          </div>
 
-      <div className={styles.student_1}>
-        <img src='/images/student1.png'></img>
+        </div>
+        <div className={styles.company_logo}>
+          <div className={styles.slider_container}>
+            <div className={styles.slider}>
+              <div className={styles.slide}>
+                <img src='/images/logo1.png'></img>
+              </div>
+              <div className={styles.slide}>
+                <img src='/images/logo2.png'></img>
+              </div>
+              <div className={styles.slide}>
+                <img src='/images/logo3.png'></img>
+              </div>
+              <div className={styles.slide}>
+                <img src='/images/logo4.png'></img>
+              </div>
+              <div className={styles.slide}>
+                <img src='/images/logo5.png'></img>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
       </div>
-    </div>
+    </>
   )
 }
